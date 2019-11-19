b71b6056927e51a36e45c1432fd2c6955e7326b9
luoqian <szuluoqian@163.com>
2019-11-19 11:44:29 +0800

    feat(package.json): 规范 start and build

--- example/index.tsx
+++ example/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as redux from '../dist/lib/redux';
 
+redux.sayHi('hello');
+
 const App = () => {
   return <div>app</div>;
 };
